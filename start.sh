d8964c3ee2a3d12997c3db997027bcfd8c68da21
werener <lenyazhuk@yandex.ru>
2025-09-08 18:00:35 +0300

    fix: script now suitable for wsl-launch

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 FOUND_FILE=$(find $(dirname "$0") -maxdepth 2 -type f -name "main.cpp" -print -quit)
 
-if [[ -n "$FOUND_FILE" ]]
+if [[ -n "$FOUND_FILE" ]];
 then
     g++ -o build/main "$FOUND_FILE" 
-    chmod u+x $FOUND_FILE
+    chmod u+x ./build/main
     ./build/main
 else
     echo "No main.cpp" 
