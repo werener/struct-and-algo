dc1e519a1a5f0e9f54c753e87148334e4d35cc69
werener <lenyazhuk@yandex.ru>
2025-09-10 17:03:00 +0300

    task 5

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
+#include <cstddef>
 #include <cstdlib>
 #include <iostream>
 #include <bitset>
 #include <vector>
 #include <cstring>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <random>
+
+
 #define u unsigned
 
 using std::cout;
@@ -13,12 +20,13 @@ void task_1_a_b() {
     u char num_bit;
     num_bit = 5;
 
-    x &= (~(1 << num_bit - 1));
+    x &= (~(1 << (num_bit - 1)));
 
     std::cout << "Fifth bit:  " << (int)x << std::endl;
 
     x = 255;
-    x &= (~(1 << num_bit - 1));
+    num_bit = 7;
+    x &= (~(1 << (num_bit - 1)));
 
     std::cout << "Seventh bit:  " << (int)x << std::endl;
 }
@@ -99,6 +107,80 @@ void task_2_с() {
     }
 }
 
+void generate_file(std::string path) {
+    std::fstream file(path, std::ios::out);
+    std::vector<u int> vec;
+    for (u int i = 1000000; i < 10000000; ++i)
+        vec.push_back(i);
+    auto rng = std::default_random_engine {};
+    std::shuffle(vec.begin(), vec.end(), rng);
+
+    for (u int el: vec) 
+        file << el << '\n';
+    file.close();
+}
+
+const std::string PATH_IN = "/mnt/c/Users/werener/Desktop/gitfolder/struct-and-algo/files/task5.txt";
+const std::string PATH_OUT = "/mnt/c/Users/werener/Desktop/gitfolder/struct-and-algo/files/task5_result.txt";
+
+const int N = 10000000 - 1000000;
+const int CUTOFF = N / 2;
+void task_3_real() {
+    std::fstream in(PATH_IN), out(PATH_OUT);
+    // this vector is 4.5mil bit (or ~0.54MB) 
+    std::vector<bool> bitarr(CUTOFF, 0); 
+    int element;
+    while (in) {
+        in >> element;
+        if (element < CUTOFF - 1)
+            bitarr[element - 1000000] = 1;
+    }
+    // reset pointer in file
+    in.clear();
+    in.seekg(0);
+    fill(bitarr.begin(), bitarr.end(), 0);
+    while (in) {
+        in >> element;
+        if (element >= CUTOFF + 1000000 - 1)
+            bitarr[element - (CUTOFF + 1000000)] = 1;
+    }
+    for (int i = 0; i < N; ++i)
+        if (bitarr[i])
+            out << i << "\n";
+    in.close(); out.close();
+}
+
+void task_3();
+
+
+#include <chrono>
+using namespace std::chrono;
 int main() {
-    return 0;
+    for (int i = 0; i < 5; ++i) {
+        auto start = high_resolution_clock::now();
+        task_3_real();
+        auto end = high_resolution_clock::now();
+        int duration = duration_cast<milliseconds>(end - start).count();
+        cout << i + 1 << ": " << duration << "ms" << std::endl; 
+    }
+}
+
+void task_3() {
+    std::fstream in(PATH_IN), out(PATH_OUT);
+
+    /* 1 MB = 1048576 byte = 8388608 bit
+    It is a vector<bool> with 8388608 0s
+    We need 9 * 10^6, so it's not possible
+    to fit the criteria with 7-digit numbers*/
+
+    std::vector<bool> bitarr(N, 0); 
+    int element;
+    while (in) {
+        in >> element;
+        bitarr[element - 1000000] = 1;
+    }
+    for (int i = 0; i < N; ++i)
+        if (bitarr[i])
+            out << i << "\n";
+    in.close(); out.close();
 }
