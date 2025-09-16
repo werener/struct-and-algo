2e880cc6d1cbcb88475d850eab1db80a80169f84
werener <lenyazhuk@yandex.ru>
2025-09-16 20:59:41 +0300

    refactor: cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,6 @@
 #define string std::string
 
 
-static const char upper[26] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 
-    'H', 'I', 'J', 'K', 'L', 'M', 'N', 
-    'O', 'P', 'Q', 'R', 'S', 'T', 
-    'U', 'V', 'W', 'X', 'Y', 'Z',
-};
-static const char lower[26] = {
-    'a', 'b', 'c', 'd', 'e', 'f', 'g',
-    'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't',
-    'u', 'v', 'w', 'x', 'y', 'z',
-};
 const ui64 CONSTRAINT_MIN = 100000000000;
 const ui64 CONSTRAINT_MAX = 1000000000000 - 1;
 
@@ -45,18 +33,18 @@ std::random_device rd;
 Book gen_entry() {
     std::mt19937_64 gen_key(rd());
     std::uniform_int_distribution<ui64> distribution(CONSTRAINT_MIN, CONSTRAINT_MAX);
-    string author = string(".."), title = string(1, upper[rand() % 26]);
+    string author = string(".."), title = string(1, 'A' + rand() % 26);
     //  ISBN
     ui64 ISBN = distribution(gen_key);
     //  author
-    author.insert(author.begin(), upper[rand() % 26]);
-    author.insert(author.begin() + 2, upper[rand() % 26]);
-    author.insert(author.end(), upper[rand() % 26]);
+    author.insert(author.begin(), 'A' + rand() % 26);
+    author.insert(author.begin() + 2, 'A' + rand() % 26);
+    author.insert(author.end(), 'A' + rand() % 26);
     for (int i = 0; i < 5 + rand() % 6;++i)
-        author.insert(author.end(), lower[rand() % 26]);
+        author.insert(author.end(), 'a' + rand() % 26);
     //  title
     for (int i = 0; i < 9 + rand() % 16; ++i)
-        title.insert(title.end(), lower[rand() % 26]);
+        title.insert(title.end(), 'a' + rand() % 26);
     return Book(ISBN, author, title);
 }
 
