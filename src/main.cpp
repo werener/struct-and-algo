96dafb3b73dc76a329506b007f3e515506dfbe64
werener <lenyazhuk@yandex.ru>
2025-09-16 09:35:36 +0300

    feat: add 'write to file' function; fix: remove temp files

--- src/main.cpp
+++ src/main.cpp
@@ -34,28 +34,37 @@ std::tuple<ui64, string, string> gen_entry() {
     std::mt19937_64 gen_key(rd());
     std::uniform_int_distribution<ui64> distribution(CONSTRAINT_MIN, CONSTRAINT_MAX);
     string author = string(".."), title = string(1, upper[rand() % 26]);
-    //ISBN
+    //  ISBN
     ui64 ISBN = distribution(gen_key);
     //  author
     author.insert(author.begin(), upper[rand() % 26]);
     author.insert(author.begin() + 2, upper[rand() % 26]);
     author.insert(author.end(), upper[rand() % 26]);
-    for (int i = 0; i < 5 + rand() % 5;++i)
+    for (int i = 0; i < 5 + rand() % 6;++i)
         author.insert(author.end(), lower[rand() % 26]);
     //  title
-    for (int i = 0; i < 7 + rand() % 10; ++i)
+    for (int i = 0; i < 9 + rand() % 16; ++i)
         title.insert(title.end(), lower[rand() % 26]);
 
     return std::tuple<ui64, string, string>{ISBN, author, title};
 }
 
 
-void write_to_file(bool dump_txt=false) {
-
+void write_to_file(int num_entries, string path="./files/data.dat") {
+    std::ofstream file(path, std::ios::binary);
+    ui64 ISBN;
+    string author, title;
+    for(int i = 0; i < num_entries; ++i) {
+        tie(ISBN, author, title) = gen_entry();
+        file.write((char*) &ISBN, sizeof(ui64));
+        //  \0 - terminating character
+        file.write(author.c_str(), author.length() + 1);
+        file.write(title.c_str(), title.length() + 1);
+    }
+    file.close();
 }
 
 
 int main() {
-    gen_entry();
-    gen_entry();
+    write_to_file(100);
 }
