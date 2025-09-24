682827ea14fbd46e3a26789ed603de9a4cc724f5
werener <lenyazhuk@yandex.ru>
2025-09-25 00:24:36 +0300

    better gitignore structuree

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,7 @@ struct Subscription {
     string address;
     bool valid;
 
-    Subscription() {
-        this->valid = false;
-    }
+    Subscription() { this->valid = false; }
 
     Subscription(ui32 number, string full_name, string address) {
         this->number = number;
@@ -19,6 +17,21 @@ struct Subscription {
             this->valid = false; 
     }
 
+    static Subscription generate_random() {
+        const std::string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        std::random_device rd; std::mt19937_64 gk(rd());
+
+        std::uniform_int_distribution<int> num_gen(10000, 99999);
+
+        // get name
+        std::uniform_int_distribution<char> char_gen('a', 'z');
+        string name = ".. ", address;
+        name.insert(0, char_gen(gk)); 
+        name.insert(2, char_gen(gk));
+        for (int i = 0; i < 5 + rand() % 6; ++i)
+
+        return Subscription(num_gen(gk), "", "");
+    }
     void fill(ui32 number, string full_name, string address) {
         this->number = number;
         this->full_name = full_name;
@@ -34,7 +47,7 @@ struct Subscription {
 
 
 struct HashTable {
-    static const ui32 C = 1, D = 3;
+    ui32 c, d;
 
     ui32 capacity;
     ui32 num_of_elements = 0;
@@ -46,13 +59,20 @@ struct HashTable {
 
     HashTable() { 
         capacity = 10;
+        //   make c and d coprime with capacity
+        c = capacity - 1;
+        d = capacity + 1;
         table = std::vector(capacity, Subscription());
     }
-    HashTable(int init_len) {
+
+    HashTable(ui32 init_len) {
         capacity = init_len * 2; 
+        table = std::vector(capacity, Subscription());
         for (int i = 0; i < init_len; ++i) {
-            this->insert(10000 + (rand()) % 90000, "23", "33", true);
+            this->insert(10000 + rand() % 90000, "23", "33", false);
         }
+        c = capacity - 1;
+        d = capacity + 1;
     }
 
 
@@ -63,11 +83,13 @@ struct HashTable {
     void rehash() {
         table.resize(table.size() * 2);
         capacity = table.size();
+        c = capacity - 1;
+        d = capacity + 1;
     }
     
-    void insert(ui32 number, string full_name, string address, bool initializing = false) {
-        ui32 hash_key = hashFunction(number);
-        Subscription hashed_entry = Subscription(number, full_name, address);
+    void insert(Subscription hashed_entry, bool not_initializing = true) {
+        ui32 hash_key = hashFunction(hashed_entry.number);
+
         if (!hashed_entry.valid) {
             std::cout << "Tried to hash invalid subscription:\n";
             hashed_entry.print();
@@ -75,7 +97,7 @@ struct HashTable {
             return;
         }
 
-        if (++num_of_elements > capacity * 0.75) {
+        if (++num_of_elements > capacity * 0.7) {
             std::cout << "Element limit exceeded. Rehashing.\n";
             rehash();
             std::cout << "Rehashed successfully.\nNew capacity: " << this->table.size() << "\n\n" ;
@@ -85,16 +107,21 @@ struct HashTable {
             int i = 0;
             std::cout << "Trying to prevent hash collision at " << hash_key << std::endl;
             while (table[hash_key].valid) 
-                hash_key = hashFunction(hash_key + C * ++i + D * i*i);
+                hash_key = hashFunction(hash_key + c * ++i + d * i*i);
             std::cout << "Found free key " << hash_key << "\n\n";
         }
 
         table[hash_key] = hashed_entry;
-        if (!initializing) {        
+        if (not_initializing) {
             hashed_entry.print();
             std::cout << "Successfully inserted at " << hash_key << "\n\n";
         }
-        
+    }
+
+    void insert(ui32 number, string full_name, string address, bool not_initializing = true) {
+        ui32 hash_key = hashFunction(number);
+        Subscription hashed_entry = Subscription(number, full_name, address);
+        this->insert(hashed_entry, not_initializing);
     }
 
     void print() {
@@ -113,7 +140,7 @@ struct HashTable {
 };
 
 int main() {
-    auto a = HashTable();
+    auto a = HashTable(1);
     a.insert(41282, "SVO", "332");
     a.insert(41282, "ABJ", "31241231232");
     a.insert(4128, "asdsad", "2367472y43");
