db80591d0a1e8947cc59d2ba1fbcba1f308c4877
werener <lenyazhuk@yandex.ru>
2025-10-04 20:58:01 +0300

    feat: everything
    Signed-off-by: werener <lenyazhuk@yandex.ru>

---
+++ src/Tree.hpp
@@ -0,0 +1,115 @@
+#include "lib.hpp"
+
+struct Node {
+    char value;
+    Node* left = nullptr;
+    Node* right = nullptr;
+    
+    Node() {};
+    Node(char value) {
+        this->value = value;
+
+    } 
+
+    bool is_leaf() {
+        return (left == nullptr && right == nullptr);
+    } 
+};
+
+struct Tree {
+protected:
+    Node* root = nullptr;
+
+    Node* createNode(char value) {
+        Node* newNode = new Node();
+        newNode->value = value;
+        newNode->left = newNode->right = nullptr;
+        return newNode;
+    }
+
+    Node* insert_private(Node* cur, char value) {
+        if (cur == nullptr)             
+            return createNode(value);
+        if (value < cur->value) 
+            cur->left = insert_private(cur->left, value);
+        else if (value > cur->value) 
+            cur->right = insert_private(cur->right, value);
+        return cur;
+    }
+    
+    void traverse_private(Node* cur) {
+        if (cur != nullptr) {
+            traverse_private(cur->left);
+            if (cur->value == '\t') 
+                std::cout << "\\t";
+            else if (cur->value == 0) 
+                std::cout << "\\0";
+            else
+                std::cout << cur->value;
+            std::cout << "(" << (int)cur->value << ") ";
+            traverse_private(cur->right);
+        }
+    }
+
+    ui64 sum_of_leaves_private(Node* cur) {
+        if (cur == nullptr)
+            return 0;
+
+        ui64 s = cur->value;
+        //  if cur isnt leaf, we need to add sum from his children
+        if (!cur->is_leaf()) 
+            s += sum_of_leaves_private(cur->left) + sum_of_leaves_private(cur->right);
+        return s;
+    }
+
+    int height_private(Node* root) {
+        if (root == nullptr) 
+            return 0;
+        int dl = height_private(root->left), dr = height_private(root->right);
+        return (1 + (dl > dr ? dl : dr));
+    }
+
+    void print_private(const std::string& prefix, const Node* node, bool isLeft) {
+        if( node != nullptr ) {
+            std::cout << prefix << (isLeft ? "├──" : "└──" );
+            if (node->value == '\t')
+                std::cout << "\\t";
+            else if (node->value == 0)
+                std::cout << "\\0";
+            else
+                std::cout << node->value;
+
+            std::cout << "("<< (int)node->value << ")" << std::endl;
+            print_private( prefix + (isLeft ? "│     " : "      "), node->left, true);
+            print_private( prefix + (isLeft ? "│     " : "      "), node->right, false);
+        }
+    }
+public:
+    Tree(){}
+
+    Tree(char value) {  root = new Node(value); }
+
+    Tree(string values) {
+        root = new Node(values[0]);
+        for (int i = 1; i < values.length(); ++i)
+            this->insert(values[i]);
+    }    
+
+    void insert(char value) {
+        root = insert_private(root, value);
+    }
+    void traverse() {
+        traverse_private(root);
+        std::cout << std::endl;
+    }
+    ui64 sum_of_leaves() {
+        return sum_of_leaves_private(root);
+    }
+    int height() {
+        return height_private(root);
+    }
+    void print() {
+        print_private("", root, false);
+        std::cout << std::endl;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,59 @@
-#include "lib.hpp"
+#include "Tree.hpp"
+char get_char() {
+    string _;
+    std::getline(std::cin, _);
+    return _[0];
+}
+char context_menu() {
+    std::cout 
+            << "\nChoose what to do:\n"
+            << "0. Exit\t\t"    <<  "3. Print\n"
+            << "1. Insert\t"    <<  "4. Find sum of leaves\n"
+            << "2. Traverse\t"  <<  "5. Find height\n\n";
+    return get_char();
+}
+
+
+void cli() {
+    std::cout << "Enter all characters to fill the tree:\n";
+    string _;
+    std::getline(std::cin, _);
+
+    Tree tree(_);
+    std::cout << "Tree:\n"; 
+    tree.print();
+
+    while (true) {
+        char choice = context_menu();
+        switch(choice) {
+            case '0':
+                return;
+            case '1':
+                std::cout << "Enter char to insert:\t";
+                tree.insert(get_char());
+                break;    
+            case '2':
+                tree.traverse();
+                break;
+            case '3':
+                tree.print();
+                break;
+            case '4':
+                std::cout << "Sum of all leaves is " << tree.sum_of_leaves();
+                break;
+            case '5':
+                std::cout << "Height is " << tree.height();
+                break;
+            default:
+                std::cout << "No such option\n";        
+        }
+    }
+}
 
 int main() {
     using namespace std::chrono;
     auto start = high_resolution_clock::now();
     auto end = high_resolution_clock::now();
+    cli();
 }
+
