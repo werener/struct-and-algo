57f1b4e4a522b9139b372c1263e3cb0d65e9e258
werener <lenyazhuk@yandex.ru>
2025-10-09 15:46:47 +0300

    feat: add test from the condition; add graph input from cin

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -4,8 +4,8 @@
 typedef size_t Vertex;
 
 struct Edge {
-    union { Vertex u; Vertex from; };
-    union { Vertex v; Vertex to; };
+    union { Vertex u; Vertex from; Vertex source; };
+    union { Vertex v; Vertex to; Vertex dest;};
     union { i64 w; i64 weight; };
 
     Edge() {}
@@ -19,39 +19,50 @@ struct Edge {
 
 struct Graph {
     size_t size;
-    std::vector<Vertex> vertices;
     std::vector<Edge> edges;
 
     Graph(size_t size, std::vector<string> edges={}) {
         this->size = size;
-        vertices = std::vector<Vertex>(size);
-        std::iota(vertices.begin(), vertices.end(), 0);
-        
         size_t u, v;
         i64 w;
         for (string edge : edges) {
             std::istringstream iss(edge);
             iss >> u >> v >> w;
-            if (u < size && v < size && u != v)
+            if (u <= size && v <= size && u != v)
                 this->add_edge(u, v, w);
         }
     }
 
-    void add_vertex() { 
-        vertices.push_back(size); 
-        std::cout << "Created new vertex #" << size++ << std::endl;
-    }
+    void add_vertex() { std::cout << "Created new vertex #" << size++ << std::endl; }
 
     void add_edge(Vertex u, Vertex v, i64 w) {
-        if (u == v)
+        if (u-- == v--)
             std::cout << "Trying to create a self-loop\n";
         else if (u < size && v < size)
             edges.push_back(Edge(u, v, w));
         else
             std::cout << "One of the vertices doesn't exist\n";
     }
+    void add_edge(string edge){
+        size_t u, v;
+        i64 w;
+        std::istringstream iss(edge);
+        iss >> u >> v >> w;
+        if (u <= size && v <= size && u != v)
+            this->add_edge(u, v, w);
+    }
+    
+    void add_non_directed_edge(string edge) {
+        size_t u, v;
+        i64 w;
+        std::istringstream iss(edge);
+        iss >> u >> v >> w;
+        if (u <= size && v <= size && u != v)
+            this->add_non_directed_edge(u, v, w);
+    }
+    
     void add_non_directed_edge(Vertex u, Vertex v, i64 w) {
-        if (u == v)
+        if (u-- == v--)
             std::cout << "Trying to create a self-loop\n";
         else if (u < size && v < size) {
             edges.push_back(Edge(u, v, w));
@@ -63,6 +74,36 @@ struct Graph {
 
     void print_edges() {
         for (Edge e : edges)
-            std::cout << e.u << " " << e.v << "  w = " << e.w << "\n";
+            std::cout << e.u + 1 << " " << e.v + 1 << "  w = " << e.w << "\n";
+    }
+
+    std::vector<i64> bellman_ford(Vertex source) {
+        std::vector<i64> distance(size, INT_MAX);
+        distance[source] = 0;
+        //  construct distance vector for each vertex
+        for (size_t i = 0; i < size - 1; ++i) 
+            for (const Edge e : this->edges)
+                if (distance[e.from] != INT_MAX && distance[e.from] + e.weight < distance[e.to]) 
+                    distance[e.to] = distance[e.from] + e.weight;            
+        
+        //  check for negative-weight cycles
+        for (const Edge e : this->edges) {
+            if (distance[e.from] != INT_MAX &&
+                distance[e.from] + e.weight < distance[e.to]) {
+                std::cout << "Graph contains negative weight cycle\n" << "Distance to any node is infinitely small\n";
+                std::fill(distance.begin(), distance.end(), INT_MIN);
+                return distance;
+            }
+        }
+
+        //  result
+        std::cout << "Vertex distances from " << source + 1 << "\n";
+        for (size_t i = 0; i < size; ++i) {
+            if (distance[i] == INT_MAX)
+                std::cout << source + 1 << " -> " << i + 1 << "\t\t" << "UNREACHABLE\n";
+            else
+                std::cout << source + 1 << " -> " << i + 1 << "\t\t" << distance[i] << "\n";
+        }
+        return distance;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,36 @@
 #include "Graph.hpp"
 
+Graph user_input() {
+    size_t size;    
+    std::vector<string> edges;    
+    std::cout << "Enter amount of Vertices:\t"; std::cin >> size;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Start entering your Edges, empty to exit:\n";
+    string s="00 00 00";
+    while (true) {
+        std::getline(std::cin, s);
+        if (s == "\0")
+            break;
+        edges.push_back(s);
+    }
+    return Graph(size, edges);
+}
 
 int main() {
-    using namespace std::chrono;
-    auto start = high_resolution_clock::now();
-    auto end = high_resolution_clock::now();
-    Graph g(3, std::vector<string>{"1 2 2", "0 2 -1", "0 3 229", "2 2 2"});
-    g.print_edges();
+    const std::vector<string> edges = 
+    { 
+        "1 2 23", "1 3 12",
+        "2 3 25", "2 5 22", "2 8 35", 
+        "3 4 18", 
+        "5 6 23",  "5 7 14", 
+        "6 7 24", 
+        "7 8 16" 
+    };
+
+    Graph g(8);
+    for (const string edge : edges) {
+        g.add_non_directed_edge(edge);
+    }
+    // g.print_edges();
+    g.bellman_ford(2);
 }   
