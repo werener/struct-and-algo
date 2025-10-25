23bc6036118027fca4b18513f32c2ed6d9d9bdbf
werener <lenyazhuk@yandex.ru>
2025-10-26 01:54:01 +0300

    finish writing code
    Signed-off-by: werener <lenyazhuk@yandex.ru>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,55 @@
-#include "lib.hpp"
+#include "solution.cpp"
 
-int main() {
-    using namespace std::chrono;
-    auto start = high_resolution_clock::now();
-    auto end = high_resolution_clock::now();
+int main() {    
+    /*  Indexation  */
+
+    std::cout << string(29, '-') << std::endl;    
+    std::vector<std::vector<ui64>> a(10, std::vector<ui64>(10,0));
+    for (int i = 0; i < 10; ++i) {
+        for (int j = 0; j < 10; ++j) {
+            a[i][j] = 10 * i + j;
+        }
+    }
+    std::cout << "ij" << "\n";
+    for (int i = 0; i < 7; ++i) {
+        for (int j = 0; j < 10; ++j) 
+            (a[i][j] < 10) ? std::cout << 0 << a[i][j] << " " : std::cout << a[i][j] << " ";
+        std::cout << std::endl;    
+    }
+  
+    /*  Grid structure  */
+    /*
+        o's are nodes
+        spaces are 'squares'
+        arrows are 'streets'
+    */
+   
+    std::cout << string(29, '-') << std::endl;  
+    std::vector<std::vector<char>> test = 
+    {
+        { 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'B', },
+        { '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', },
+        { 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', },
+        { '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', },
+        { 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', },
+        { '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', },
+        { 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', },
+        { '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', ' ', '^', },
+        { 'A', '>', 'o', '>', 'o', '>', 'o', '>', 'o', '>', 'o', },
+    };
+    print_matrix(test);
+    
+    /*  Test */
+    std::cout << string(29, '-') << std::endl;    
+
+    grid_type test_data = 
+    {
+        { Node(0, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1) },
+        { Node(0, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1) },
+        { Node(0, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1) },
+        { Node(0, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1), Node(1, 1) },
+        { Node(0, 0), Node(1, 0), Node(1, 0), Node(1, 0), Node(1, 0), Node(1, 0) },
+    };
+    ui64 res = find_optimal_path(test_data);
+    std::cout << "Answer:\t" << res << std::endl;
 }
---
+++ src/solution.cpp
@@ -0,0 +1,56 @@
+#include "lib.hpp"
+
+template <typename T>
+void print_matrix(T matrix) {
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; j < matrix[0].size(); ++j) 
+            std::cout << matrix[i][j] << " ";
+        std::cout << std::endl;    
+    }
+}
+
+struct Node {
+    union { ui64 least_time=0; ui64 t; };
+    union { ui16 from_south; ui16 fs;};
+    union { ui16 from_west; ui16 fw; };
+    Node(ui16 fw_, ui16 fs_) {from_south = fs_; from_west = fw_;}
+
+    friend std::ostream& operator<<(std::ostream& os, const Node& obj) {
+        os << obj.t;
+        return os;
+    }
+};
+
+typedef std::vector<std::vector<Node>> grid_type;
+
+
+
+ui64 find_optimal_path(grid_type grid) {
+    //  [lh-1][0] is A     [0][lw-1] is B
+    int lh = grid.size();
+    int lw = grid[0].size();
+
+    //  paths along border can be visited only one way
+    grid[lh - 1][0].t = 0;  //  A
+    for (int i = lh - 2; i >= 0; --i)   //  east border
+        grid[i][0].least_time = grid[i + 1][0].t + grid[i][0].fs; 
+    
+    for (int j = 1; j < lw; ++j)        //  south border
+        grid[lh - 1][j].least_time = grid[lh - 1][j - 1].t + grid[lh - 1][j].fw;
+
+    //  dynamically updating every cells' fastest route 
+    for (int i = lh - 2; i >= 0; --i) {
+        for (int j = 1; j < lw; ++j) {
+            Node *current = &grid[i][j];
+            ui64 from_south = grid[i + 1][j].t + current->fs;
+            ui64 from_west = grid[i][j - 1].t + current->fw;
+
+            current->least_time = (from_south < from_west) ? from_south : from_west;
+        }
+    }
+    std::cout << string(lw / 2, ' ') << lw << " x " << lh << '\n';
+    print_matrix(grid);
+    return grid[0][lw - 1].least_time;
+}
+
+
